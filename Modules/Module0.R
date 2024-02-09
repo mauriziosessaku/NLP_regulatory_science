d960369c2105eb01d93acce337ffce8b051c0ed4
saeedshak <42966876+saeedshak@users.noreply.github.com>
2024-02-09 13:05:34 +0100

    Update Module0.R

--- Modules/Module0.R
+++ Modules/Module0.R
@@ -1,1 +1,36 @@
+#############################################################################################
+#Module 0: Libraries and function
+#Version 1.0 
+#Last change: 09/02/2024
+#Authors: Saeed Shakibfar & Maurizio Sessa
+#############################################################################################
 
+library(readxl)
+library(rvest)
+library(tidyverse)
+library(xml2)
+library(rvest)
+library(xml2)
+library(plyr)
+library(data.table)
+library(tm)
+library(pdftools)
+library(stringr)
+library(lubridate)
+library(anytime)
+library(qdapRegex)
+library(sjmisc)
+library(fs)
+library(berryFunctions)
+library(textclean)
+library(sys)
+library(writexl)
+library(arsenal)
+library(magick)
+library(tesseract)
+library(dplyr)
+library(arsenal)
+
+###########################################
+# END
+###########################################
