3d5510768842781b3457442497aa1e5c4d2ee3e5
Saeed Shakibfar <42966876+saeedshak@users.noreply.github.com>
2024-05-30 23:31:16 +0200

    Update Module4_Pharmaceutical_forms.R

--- Modules/Module4_Pharmaceutical_forms.R
+++ Modules/Module4_Pharmaceutical_forms.R
@@ -6,12 +6,16 @@
 #############################################################################################
 setwd(path2)
 
+
 for(i in 1:nrow(wb))   {  
  
   lines <- unlist(stringr::str_split(pdftools::pdf_text(wb[i,]), "\n"))
+  # 
   start <- stringr::str_which(lines, "Scope of the application")
+
   
   if (length(start) == 1 ){
+
   test <- lines[start:(start+60)]
   res<-data.frame(str_detect(test,"Pharmaceutical form"))
   colnames(res)<-"Result"
@@ -20,8 +24,12 @@ for(i in 1:nrow(wb))   {
     for (j in  nrow(res):1){ 
       res2<-as.numeric(row.names(res)[j])
        if (str_detect(test[res2],":")){
+        #res2<-res2+1
         a<-test[res2]
+        
+        #a<-test[res2+1]
         for (jj in 1: 10){ 
+          
           a2 <- str_trim( test[res2+jj], "left")
           if (!is.na(a2)){
           if (a2 !=""){
@@ -45,9 +53,12 @@ for(i in 1:nrow(wb))   {
         a <- str_trim(a, "right")
         a <- str_trim(a, "left")
         df2[i,14] <- a
+        
+    }
   }
   }
-  }
+  
+  
   }else {
     
     start <- stringr::str_which(lines, "Pharmaceutical form")
@@ -55,24 +66,33 @@ for(i in 1:nrow(wb))   {
     for (j in 1:length(start)) {
       if (substr(lines[start[j]], nchar(lines[start[j]]), nchar(lines[start[j]]))==":"){
         test <- lines[start[j]:(start[j]+30)]
+        
         res<-data.frame(str_detect(test,"Pharmaceutical form"))
         colnames(res)<-"Result"
         res<-subset(res,res$Result==TRUE)
         res2<-as.numeric(row.names(res)[1])
         if (!is.na(res2)){
+          
           a<-test[res2]
+          
           for (jj in 1: 10){ 
             a2 <- str_trim( test[res2+jj], "left")
+            
             if (a2 !=""){
               a<-paste0(a,"; ",a2)
             }
-
+            
+            
             if (substr(test[res2+jj+1], nchar(test[res2+jj+1]), nchar(test[res2+jj+1]))==":" & nchar(test[res2+jj+1])<50){
               break
+              
             }
             if (test[res2+jj]==''& test[res2+jj+1]==''){
               break
+              
             }
+            
+            
           }
           a<-stringi::stri_replace_first_fixed(a, ":;", "XXX")
           a<-sub(".*XXX","",a)
@@ -80,13 +100,20 @@ for(i in 1:nrow(wb))   {
           a <- str_trim(a, "right")
           a <- str_trim(a, "left")
           df2[i,14] <- a
-        }      
+          
+        }
+        
+        
+        
         break
       }
+    }
+    
     } 
-    } 
+    
   }
   }
+
 
 setwd(Path1)
 
